d9de7164bb070990625dcb4ab4709aafadf60056
Kanan <kanan.31352762684@icloud.com>
2020-09-28 19:38:26 +0900

    change mistake

--- project3/blog/templatetags/mytag.py
+++ project3/blog/templatetags/mytag.py
@@ -5,5 +5,5 @@ register = template.Library()
 @register.simple_tag
 def url_replace(request, field, value):
     url_dict = request.GET.copy()
-    url_dict[field] = value
+    url_dict[field] = str(value)
     return url_dict.urlencode()
